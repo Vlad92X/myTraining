69512fd64b0873178727a7b070ac234ad2267edc
Vlad Khodakov <Vlad92X@gmail.com>
2019-10-11 16:56:24 +0300

    my initial job!

---
+++ C++_projects/Elementary/Audi/Audi/Audi.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib> // для system
+using std::cout;
+using std::endl;
+
+int main()
+{
+	setlocale(LC_CTYPE, "rus");
+	system("mode con cols=150 lines=80");// изменение размера окна консоли
+
+	
+	cout << "\n\n"<<"\t\t\t\t\t\t\tAUDI\n \n \n " << endl;
+	
+	cout << " \t\t\t\t\t\   .,=a*fMMMMMMMMMMMMMMt**aa,.. " << endl;
+	cout << "\t\t\t\t       .,=*fY\".................\'\"\"aTWWKb*aa,.. " << endl;
+	cout << "\t\t\t\t   .a*fO*\"............................,\'\"++KKMMb*a. " << endl;
+	cout << "\t\t\t        .afOV\",..................................+++++\"*VKKb*a. " << endl;
+	cout << "\t\t\t     .afMV,soooo..................................++++++++\"ffKKb*a-,. " << endl;
+	cout << "\t\t\t  .afMM**,,soo*___________________________________+++++++++*fffLLKKb***a--,... " << endl;
+	cout << "\t      .,-aaa**fMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW#MWo=========MM##MM#############ba,." << endl;
+	cout << " \t ..afMMM*\"*WMMY*====*VHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHWKKWoo........**########V**M######MMMMA," << endl;
+	cout << "      ..afM+,,++,,.WV\"" << ".a***a.. \'YMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMV#Ws\'..,,,,..######H\"" << ".a***a.. \'Y#####Gb" << endl;
+	cout << "    .fMYP\',,.,,.fWW\'" << " aHKKKKOOLL. " << "VMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM##MMW*\'oooooooo#####W\'" << " aHKKKKOOLL. " << "VM##KHKb" << endl;
+	cout << "   fHHY\"\"\'\'\'\'\"fMW." << " awvkssooowkOOL " << "\"YMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM#KWoooooooooAMM#W." << " awvkssooowkOOL " << "\"YH##HHb" << endl;
+	cout << "   HH\"\'    \"\".MW\'" << " aLLLLoossaawwHHb " << "\"MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM#WaaoooooooJ###W\'" << " aLLLLoossaawwHHb " << "\"MM###bo" << endl;
+	cout << "   HH\'      \'MMM." << " XMMaass****wwLLOO " << "MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWpooosswwJH###M." << " XMMaass****wwLLOO " << "MH####b" << endl;
+	cout << "   HH      ooMMM." << " MMaass****wwLLOOs " << "XMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM*oo**ffQ#####MM." << " MMaass****wwLLOOs " << "XHHHHHP" << endl;
+	cout << "   HHwwaaKK#####" << "  VH##********aaKK\'"<< ".#########################HHHHHHHH#############M#," << " VH##********aaKK\'" << ".#HHW*"<< endl;
+	cout << "   \"*WMMMMMMMMMM." << "  YOOLLaaoowwKK## ,oossoooossssssssssssssaassssssssssssaaaaaaaaasaM." << "  YOOLLaaoowwKK## ,oHP\'  " << endl;
+	cout << "                    Y**Yaawwss***\'                                                      Y**Yaawwss***\'   " << endl;
+	cout << "                     \',*Y,**,,*\'                                                         \',*Y,**,,*\'  " << endl;
+	cout << "________________________.____._____________________________________________________________._____.__________________" << endl;
+	cout << "\t\n" << endl;
+	cout << "\n" << endl;
+
+	system("pause");
+	return 0;
+}
