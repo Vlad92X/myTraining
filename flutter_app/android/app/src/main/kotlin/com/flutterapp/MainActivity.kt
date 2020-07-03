b07e1b38b112eb82bd3eb8e80b1203854075fc4e
Vlad92X <Vlad92X@gmail.com>
2020-07-03 14:27:29 +0300

    commit - today 03.07.2020. work in flutter

--- flutter_app/android/app/src/main/kotlin/com/flutterapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.flutterapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter_app_test/android/app/src/main/kotlin/com/flutterTest/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.flutterTest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
